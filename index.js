e9bc905a242c24fedf99dfac93c24f633cb11d04
Daniel <KunitskiiDanik@mail.ru>
2021-11-18 23:13:49 +0300

    add variable

---
+++ index.js
@@ -0,0 +1,17 @@
+const getStart = document.querySelector('#start');
+
+const tagPlus = document.querySelectorAll('button');
+const expAdd = tagPlus[0];
+const incomeAdd = tagPlus[1];
+
+
+const checkBox = document.querySelector('#deposit-check');
+
+const inputIncome = document.querySelectorAll('.additional_income-item');
+
+const budgetDayValue = document.querySelectorAll('.budget_day-value');
+const expensesValue = document.querySelectorAll('.expenses_month-value');
+const additionaIncomelValue = document.querySelectorAll('.additional_income-value');
+const additionaExpenseslValue = document.querySelectorAll('.additional_expenses-value');
+const incomeValue = document.querySelectorAll('.income_period-value');
+const targetValue = document.querySelectorAll('.target_month-value');
